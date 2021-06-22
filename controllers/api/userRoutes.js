aa6e748571a70a6f7ae2d938caee7d0f064b0bb0
Jose Orozco <jose816orozco@gmail.com>
2021-06-22 18:29:27 -0500

    done with routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,10 +9,10 @@ router.post('/', async (req, res) => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
-      res.status(200).json(userData);
+      res.json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
@@ -38,6 +38,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.username = userData.username
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
